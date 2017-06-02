8d1b45187f367673fb5e652e5cfa922bfd364d64
CaSSoSSiSSe <yoyo250998@gmail.com>
2017-06-02 09:59:18 +0200

    Phase 2

---
+++ src/jpu2016/dogfight/controller/DogFightController.java
@@ -0,0 +1,34 @@
+package jpu2016.dogfight.controller;
+
+public class DogFightController implements IOrderPerformer {
+
+	private static int TIME_SLEEP = 30;
+	private final IDogFightModel doghtfightModel;
+
+
+	public DogFightController(final IDogFightModel doghtfightModel){
+		this.doghtfightModel = doghtfightModel;
+	}
+
+	@Override
+	public void orderPerform(final UserOrder userOrder) {
+
+	}
+
+	public void play(){
+
+	}
+
+	public void setViewSystem(final IViewSystem viewSystem){
+
+	}
+
+	private void launchMissile(final int player){
+
+	}
+
+	private void gameLoop(){
+
+	}
+
+}
