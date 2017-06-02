b5961b1a23519f829e7e9515f5fa5b155d4c6c09
CaSSoSSiSSe <yoyo250998@gmail.com>
2017-06-02 10:53:42 +0200

    Main

---
+++ src/DogFightMain.java
@@ -0,0 +1,12 @@
+
+public class DogFightMain {
+
+	public static void main(final String[] args) {
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play();
+	}
+
+}
